96c3a160f8e4b92999623e3c1108c6f615b8aeea
Winner <158045429+codinglife108@users.noreply.github.com>
2024-08-05 13:20:00 -0400

    feat: update logic for pool and maker

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require("./db");
 const { getPrice } = require("./util");
 const { TrxEventDetails } = require("./model");
 
-const decimal = 1000000000 // 9 of 10
+const decimal = 1000000000; // 9 of 10
 
 async function fetchTransaction(tx) {
   try {
@@ -18,15 +18,28 @@ async function fetchTransaction(tx) {
       maxSupportedTransactionVersion: 1,
     });
 
-    const { postTokenBalances, preTokenBalances } = transaction.meta;
+    const { postTokenBalances, preTokenBalances, postBalances, preBalances } =
+      transaction.meta;
 
-    const balanceData = []
+    const balanceData = [];
 
-    for (let i = 0 ; i < postTokenBalances.length; i++) { 
-      if (postTokenBalances[i].mint == 'So11111111111111111111111111111111111111112' && postTokenBalances[i].uiTokenAmount.amount == 0) {
-        break
+    for (let i = 0; i < postTokenBalances.length; i++) {
+      if (
+        postTokenBalances[i].mint ==
+          "So11111111111111111111111111111111111111112" &&
+        postTokenBalances[i].uiTokenAmount.amount == 0
+      ) {
+        continue;
       }
-      const matchedPre = preTokenBalances.find(t => t.accountIndex == postTokenBalances[i].accountIndex)
+      if (
+        postTokenBalances[i].mint ==
+        "CGKtv3vELziHAjrDj919yymXxyyhJury37TDQJHuXjSF"
+      ) {
+        continue;
+      }
+      const matchedPre = preTokenBalances.find(
+        (t) => t.accountIndex == postTokenBalances[i].accountIndex
+      );
       balanceData.push({
         mint: postTokenBalances[i].mint,
         owner: postTokenBalances[i].owner,
@@ -35,34 +48,40 @@ async function fetchTransaction(tx) {
       });
     }
 
-    console.log(balanceData)
+    console.log(balanceData, postBalances[0], preBalances[0]);
 
     // If First token change amount is minus, it is buy, otherwise it is sell
-    const type = balanceData[0].postamount - balanceData[0].preamount > 0 ? 'sell' : 'buy'
+    const type =
+      balanceData[0].postamount - balanceData[0].preamount > 0 ? "buy" : "sell";
 
-    const quoteToken = balanceData.find(t => t.mint == 'So11111111111111111111111111111111111111112')
-    const baseAmount = Math.abs(balanceData[0].postamount - balanceData[0].preamount)
-    const quoteAmount = Math.abs(quoteToken.postamount - quoteToken.preamount)
-    const price = baseAmount / quoteAmount
-    const maker = type == 'sell' ? balanceData[0].owner : quoteToken.owner
+    const quoteToken = balanceData.find(
+      (t) => t.mint == "So11111111111111111111111111111111111111112"
+    );
+    const baseAmount = Math.abs(
+      balanceData[0].postamount - balanceData[0].preamount
+    );
+    const quoteAmount = Math.abs(quoteToken.postamount - quoteToken.preamount);
+    const price = quoteAmount / baseAmount;
 
     const saveData = {
       eventDisplayType: type, // buy or sell
       baseToken: balanceData[0].mint, // base token address - index 1 mint
-      quoteToken: quoteAmount.mint, // quote token address - index 2 mint
+      quoteToken: quoteToken.mint, // quote token address - index 2 mint
       amountInUsd: 0, // base amount usd value
       amountOutUsd: 0, // quote amount usd value
       token0ValueBase: baseAmount / decimal, // base amount
       token1ValueQuote: quoteAmount / decimal, // quote amount
       timestamp: transaction.blockTime, // timestamp
       transactionHash: tx, // transactionHash
-      maker: maker, // creator of trx - index 1 owner
-      poolAddress: balanceData[3].owner, // poolAddress - index 7 owner
+      maker: balanceData[0].owner, // creator of trx - index 1 owner
+      poolAddress: quoteToken.owner, // poolAddress - index 7 owner
       price: price, // price swapped at
     };
 
-    await TrxEventDetails.create(saveData)
-    
+    console.log(saveData);
+
+    await TrxEventDetails.create(saveData);
+
     console.log("Saving transaction data to database...");
   } catch (err) {
     console.error(err, "fetchTransaction Error");
@@ -88,25 +107,30 @@ async function getRecentTransactions(routerAddress) {
 
 function subscribeToTransactions() {
   const publicKey = new PublicKey(TOKEN_ADDRESS);
-  connection.onLogs(publicKey, (log) => {
-    console.log("New transaction log:", log);
-    fetchTransaction(log.signature);
-  });
+  connection.onLogs(
+    publicKey,
+    (log) => {
+      console.log("New transaction log:", log);
+      fetchTransaction(log.signature);
+    },
+    "finalized"
+  );
 }
 
 async function main() {
-  setTimeout(() => {
+  setTimeout(async () => {
     // At first time, we will have to test with recent transactions
     // getRecentTransactions(TOKEN_ADDRESS);
 
     // When deploy, will enable subscribeToTransactions function to get transaction realtime log
     // subscribeToTransactions();
-    
-    // This is for get USDC price for each token
-    // getPrice()
 
+    
     // First time, I tested with this transaction
-    // fetchTransaction('5YhEiWHARgyguJ9dSsdCUmQ4S6puuJS2UXGXQK2KQesjqPCwii7iffJBZLMjGfmrnWYfbNXb5dGDmfF7U1Mq99oy');
+    await fetchTransaction('5YhEiWHARgyguJ9dSsdCUmQ4S6puuJS2UXGXQK2KQesjqPCwii7iffJBZLMjGfmrnWYfbNXb5dGDmfF7U1Mq99oy');
+
+    // This is for get USDC price for each token
+    getPrice()
   }, 3000);
 }
 
--- util.js
+++ util.js
@@ -1,5 +1,14 @@
 exports.getPrice = async () => {
-    const res = await fetch('https://price.jup.ag/v4/price?ids=BLLbAtSHFpgkSaUGmSQnjafnhebt8XPncaeYrpEgWoVk,So11111111111111111111111111111111111111112')
-    const data = await res.json()
-    console.log(data)
-} 
+  const res = await fetch(
+    "https://price.jup.ag/v4/price?ids=BLLbAtSHFpgkSaUGmSQnjafnhebt8XPncaeYrpEgWoVk,So11111111111111111111111111111111111111112"
+  );
+  const { data } = await res.json();
+
+  console.log(
+    data,
+    data["BLLbAtSHFpgkSaUGmSQnjafnhebt8XPncaeYrpEgWoVk"] /
+      data["So11111111111111111111111111111111111111112"],
+    "updated price"
+  );
+  // console.log(data["BLLbAtSHFpgkSaUGmSQnjafnhebt8XPncaeYrpEgWoVk"] / data['So11111111111111111111111111111111111111112'], 'updated price')
+};
